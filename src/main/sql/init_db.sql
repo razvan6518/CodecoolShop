25777c3902b814b5aa6dbff456120099ffddb1fc
razvan6518 <razvan.raindl2@gmail.com>
2021-12-20 12:28:14 +0200

    Create init_db.sql

---
+++ src/main/sql/init_db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS products (
+                                        id serial PRIMARY KEY,
+                                        default_price bigint,
+                                        default_currency JSON,
+                                        product_category JSON,
+                                        supplier JSON
+);
+
+CREATE TABLE IF NOT EXISTS product_categories (
+                                        id serial PRIMARY KEY,
+                                        department varchar,
+                                        products JSON
+);
+
+CREATE TABLE IF NOT EXISTS suppliers (
+                                         id serial PRIMARY KEY,
+                                         department varchar,
+                                         products JSON
+);
