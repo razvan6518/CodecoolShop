4a468ba7ebf55e44fa58725f12e2326343c24c6a
razvan6518 <razvan.raindl2@gmail.com>
2022-01-04 16:35:08 +0200

    Implement payment(need code refactor and CSS)

--- src/main/java/com/codecool/shop/controller/Payment.java
+++ src/main/java/com/codecool/shop/controller/Payment.java
@@ -3,6 +3,9 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.Order;
+import com.codecool.shop.util.StripeApi;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.PaymentMethod;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,8 +13,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @WebServlet(name = "Payment", urlPatterns = {"/payment"})
@@ -30,7 +35,40 @@ public class Payment extends HttpServlet {
             totalPrice = totalPrice.add(item.getItem().getDefaultPrice().multiply(BigDecimal.valueOf(item.getQuantity() * 1.0)));
         context.setVariable("total_price", totalPrice);
 
-        engine.process("product/payment.html", context, response.getWriter());
+        PaymentIntent paymentIntent = StripeApi.createPaymentIntent(totalPrice.setScale(0, RoundingMode.UP).intValueExact(), "USD");
+        HttpSession httpSession = request.getSession();
+        httpSession.setAttribute("paymentIntent", paymentIntent);
+
+        engine.process("payment/payment.html", context, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Order order = Order.getInstance();
+        List<LineItem> productsList = order.getProductsInCart();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+
+        BigDecimal totalPrice = new BigDecimal("0");
+        for (LineItem item: productsList)
+            totalPrice = totalPrice.add(item.getItem().getDefaultPrice().multiply(BigDecimal.valueOf(item.getQuantity() * 1.0)));
+
+        context.setVariable("total_price", totalPrice);
+
+        PaymentMethod paymentMethod = StripeApi.createPaymentMethod(request.getParameter("card_number"), Integer.parseInt(request.getParameter("exp_month")), Integer.parseInt(request.getParameter("exp_year")), request.getParameter("cvc"));
+        if (paymentMethod != null){
+            HttpSession httpSession = request.getSession();
+            PaymentIntent paymentIntent = (PaymentIntent) httpSession.getAttribute("paymentIntent");
+            PaymentIntent updatedPaymentIntent = StripeApi.tryToConfirmPayment(paymentIntent, paymentMethod.getId());
+            if (updatedPaymentIntent.getStatus().equals("succeeded")){
+                engine.process("payment/succeeded.html", context, response.getWriter());
+            }else{
+                engine.process("payment/payment.html", context, response.getWriter());
+            }
+        }else{
+            engine.process("payment/payment.html", context, response.getWriter());
+        }
     }
 
 }
--- src/main/java/com/codecool/shop/util/StripeApi.java
+++ src/main/java/com/codecool/shop/util/StripeApi.java
@@ -1,12 +1,15 @@
 package com.codecool.shop.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.stripe.Stripe;
 import com.stripe.exception.*;
 import com.stripe.model.Customer;
-import com.stripe.net.RequestOptions;
+import com.stripe.model.PaymentIntent;
+import com.stripe.model.PaymentMethod;
 
 public class StripeApi {
 
@@ -16,10 +19,66 @@ public class StripeApi {
         Customer customer = null;
         try {
             customer = Customer.create(params);
-        } catch (AuthenticationException | APIException | CardException | APIConnectionException | InvalidRequestException e) {
+        } catch (StripeException e) {
             e.printStackTrace();
         }
         return customer;
+    }
+
+    public static PaymentIntent createPaymentIntent(int amount, String currency){
+        Stripe.apiKey = "sk_test_51KE7W8G8Vd74GxWdplerbvkihsXMkxu4Vab7z1rnsvsYW822n5mhn08r9dqpFi6FGJNdpmpeRqxAaEjGg7w0C3qY00tZ1NyFGX";
+
+        List<Object> paymentMethodTypes =
+                new ArrayList<>();
+        paymentMethodTypes.add("card");
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", amount);
+        params.put("currency", currency);
+        params.put(
+                "payment_method_types",
+                paymentMethodTypes
+        );
+        PaymentIntent paymentIntent = null;
+        try {
+            paymentIntent = PaymentIntent.create(params);
+        } catch (StripeException e) {
+            e.printStackTrace();
+        }
+        return paymentIntent;
+    }
+
+    public static PaymentMethod createPaymentMethod(String number, int expMonth, int expYear, String cvc){
+        Stripe.apiKey = "sk_test_51KE7W8G8Vd74GxWdplerbvkihsXMkxu4Vab7z1rnsvsYW822n5mhn08r9dqpFi6FGJNdpmpeRqxAaEjGg7w0C3qY00tZ1NyFGX";
+
+        Map<String, Object> card = new HashMap<>();
+        card.put("number", number);
+        card.put("exp_month", expMonth);
+        card.put("exp_year", expYear);
+        card.put("cvc", cvc);
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", "card");
+        params.put("card", card);
+        PaymentMethod paymentMethod = null;
+        try {
+            paymentMethod = PaymentMethod.create(params);
+        } catch (StripeException e) {
+            e.printStackTrace();
+        }
+        return paymentMethod;
+    }
+
+    public static PaymentIntent tryToConfirmPayment(PaymentIntent paymentIntent, String paymentMethodId){
+        Stripe.apiKey = "sk_test_51KE7W8G8Vd74GxWdplerbvkihsXMkxu4Vab7z1rnsvsYW822n5mhn08r9dqpFi6FGJNdpmpeRqxAaEjGg7w0C3qY00tZ1NyFGX";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("payment_method", paymentMethodId);
+        PaymentIntent updatedPaymentIntent = null;
+        try {
+            updatedPaymentIntent = paymentIntent.confirm(params);
+        } catch (StripeException e) {
+            e.printStackTrace();
+        }
+        return updatedPaymentIntent;
     }
 
 }
